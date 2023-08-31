55a24b532e26a515e50ba8dd19092d6eaaed1146
bomby5 <65233901+bomby5@users.noreply.github.com>
2023-08-31 05:41:36 -0400

    bugfixing

--- Anoikis.py
+++ Anoikis.py
@@ -1,8 +1,20 @@
 URL = "http://anoik.is/systems/"
+ZURL = "https://zkillboard.com/system/"
+import time as t
 import webbrowser as w
 
 while True:
     system = input("")
-    fURL = (URL+system)
+    if system.lower() == "thera":
+        w.open("http://anoik.is/systems/Thera")
+    elif len(system) != 7:
+        print("Invalid System Name")
+        continue
+    elif system[0].lower() != "j":
+        print ("Invalid System Name")
+        continue
+    else:
+        fURL = (URL+system.upper())
+        w.open(fURL)
+        t.sleep(0.5)
 
-    w.open(fURL)
