c31bf3c2bb38f9a4a60ea5c8883dfff739a832d9
myusernamesthis forever <Ashy>
2024-11-14 14:13:38 -0500

    Added support for Ellatha and built to EXE

--- Anoikis.py
+++ Anoikis.py
@@ -5,6 +5,7 @@
 #thanks :)
 
 URL = "http://anoik.is/systems/"
+elURL = "https://www.ellatha.com/eve/WormholeSystemview.asp?key="
 dURL = "http://evemaps.dotlan.net/system/"
 zkURL = "https://zkillboard.com/system/"
 import time as t
@@ -14,16 +15,20 @@ def openSystem(systemname):
     if systemname == "Thera":
         fURL = (URL+"Thera")
         f_d_URL = (dURL+"Thera")
+        f_el_URL = (elURL+"Thera")
         w.open(f_d_URL)
         w.open(fURL)
+        w.open(f_el_URL)
     elif systemname[0].lower() != "j" or len(systemname) != 7:
         f_d_URL = (dURL+systemname)
         w.open(f_d_URL)
     else:
         fURL = (URL+systemname.upper())
         f_d_URL = (dURL+systemname.upper())
+        f_el_URL = (elURL+systemname.upper())
         w.open(f_d_URL)
         w.open(fURL)
+        w.open(f_el_URL)
 
 
 print("Ctrl+C to exit")
@@ -36,3 +41,4 @@ try:
 
 except KeyboardInterrupt:
     print("Exiting")
+    t.sleep(1)
