b51f6f7954602b739637b57f92e83e0e3ae4c4cb
myusernamesthis forever <Ashy>
2024-11-14 15:08:48 -0500

    FINALLY added Zkillboard support! Enjoy!

--- Anoikis.py
+++ Anoikis.py
@@ -10,18 +10,34 @@ dURL = "http://evemaps.dotlan.net/system/"
 zkURL = "https://zkillboard.com/system/"
 import time as t
 import webbrowser as w
+import requests as req
+import json
+
+def getSystemId(systemname):
+    json_data = [
+        systemname,
+    ]
+    raw = req.post("https://esi.evetech.net/latest/universe/ids/?datasource=tranquility&language=en", json=json_data)
+    jsonv = json.loads(raw.text)
+    for i in jsonv['systems']:
+        return(str(i['id']))
+
 
 def openSystem(systemname):
-    if systemname == "Thera":
+    if systemname.lower() == "thera":
         fURL = (URL+"Thera")
         f_d_URL = (dURL+"Thera")
         f_el_URL = (elURL+"Thera")
         w.open(f_d_URL)
         w.open(fURL)
         w.open(f_el_URL)
+        f_zkURL = (zkURL+getSystemId("Thera"))
+        w.open(f_zkURL)
     elif systemname[0].lower() != "j" or len(systemname) != 7:
         f_d_URL = (dURL+systemname)
         w.open(f_d_URL)
+        f_zkURL = (zkURL+getSystemId(systemname))
+        w.open(f_zkURL)
     else:
         fURL = (URL+systemname.upper())
         f_d_URL = (dURL+systemname.upper())
@@ -29,6 +45,8 @@ def openSystem(systemname):
         w.open(f_d_URL)
         w.open(fURL)
         w.open(f_el_URL)
+        f_zkURL = (zkURL+getSystemId(systemname))
+        w.open(f_zkURL)
 
 
 print("Ctrl+C to exit")
@@ -37,7 +55,6 @@ try:
         t.sleep(0.5)
         system = input("")
         openSystem(system)
-#TODO: Zkill Integration(how do i convert systems to system ids)
 
 except KeyboardInterrupt:
     print("Exiting")
